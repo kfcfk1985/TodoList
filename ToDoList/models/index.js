bdbf8d512d093f83f06926f75d7953f50b35d8c6
ldf <kfcfk1985@163.com>
2020-03-19 16:58:41 +0800

    增加注析

--- ToDoList/models/index.js
+++ ToDoList/models/index.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+const basename = path.basename(__filename);        //结果为:index.js
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 console.log(`在 index.js中， config  的值为${JSON.stringify(config)}\n`)
@@ -21,9 +21,13 @@ if (config.use_env_variable) {
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (   (file.indexOf('.') !== 0))       //带有 "."
+            && (file !== basename)              //不为 index.js
+            && (file.slice(-3) === '.js');      //且为 ".js" 的后续
   })
   .forEach(file => {
+
+    //见：https://sequelize.org/v5/manual/models-definition.html#import
     const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
--- ToDoList/models/todo.js
+++ ToDoList/models/todo.js
@@ -1,4 +1,6 @@
 module.exports = function (sequelize, DataTypes) {
+
+    // 定义模型
     var Todo = sequelize.define("Todo", {
         text: DataTypes.STRING,
         complete: DataTypes.BOOLEAN
